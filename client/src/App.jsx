09f3dd5fb142e582262799bbee23d7fcb57163c5
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-03-03 22:26:38 +0100

    removed clutter - fresh landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to Collab Finance 💰</h1>
+    </div>
+  );
+}
+
+export default App;
