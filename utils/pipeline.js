e9eac69c616c4eb7b120ba5972d6c14cee84a76e
Joshua Oladeji <dejijolaoluwa@gmail.com>
2022-03-11 19:44:44 +0100

    added cors to application pipeline

--- utils/pipeline.js
+++ utils/pipeline.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 const homeRouter = require('../routes/home.js');
 const errorRouter = require('../routes/error.js');
 const userRoutes = require('../routes/user.js');
@@ -11,6 +12,7 @@ const errorController = require('../controllers/errorController.js')
 const pipeline = (app) => {
 
   // middlewares
+  app.use(cors());
   app.use(express.json());
   app.use(helmet());
   app.use(morgan('short'));
